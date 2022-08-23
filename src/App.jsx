d8548b5152d2e339db43450fb15e09ae6c998721
Vitalij <vitalij.cernysiov@gmail.com>
2022-08-23 23:07:05 +0300

    class

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
 import './App.scss';
+import Dog from './Components/011/Dog';
 
-const dogs = ['šuo', 'šunius', 'Bobikas', 'kudlius', 'Šarikas', 'avigalvis'];
- 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Props</h1>
-         <div className="dog-bin">
-            {
-                dogs.map((d, i) => <div key={i} className={'dog ' + (i % 2 ? 'sq' : 'cc')} >{d}</div>)
-            } 
-        </div>
-           <div className="dog-bin">
-            {
-                dogs.map((d, i) => (d[0] === d[0].toLocaleLowerCase()) ? <div key={i} className="dog sq">{d}</div> : null)
-            }
-         </div>
-
-         <div className="dog-bin">
-            {
-                dogs.map((d, i) => <div style={{color: d.length > 6 ? 'greenyellow' : 'crimson'}} key={i} className="dog sq">{d.length}</div>)
-            }
-        </div>
+        <h1>State</h1>
+        <Dog color="crimson"/>
       </header>
     </div>
   ); 
