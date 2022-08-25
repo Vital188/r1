43ed4a1647f40cf56beed93cbe70e55ce06274b9
Vitalij <vitalij.cernysiov@gmail.com>
2022-08-25 09:59:08 +0300

    22

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useState } from 'react';
 import './App.scss';
 
+const [count, setCount] = useState(3);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>STATE</h1>
+      <h1>Count: {count}</h1>
+      <h2>SET {setCount}</h2>
+      <button>M1</button>
+      <button>M2</button>
       </header>
     </div>
   ); 
---
+++ src/Components/013/Migtukai.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function Migtukai () {
+
+   
+
+return 
+
+       
+
+}
+
+
+export default Migtukai;
