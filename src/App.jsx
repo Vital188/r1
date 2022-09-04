0c82ec34aa776841fb77fe8ebf06df42eacd24cf
Vitalij <vitalij.cernysiov@gmail.com>
2022-09-04 15:30:41 +0300

    react app for review

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-<<<<<<< HEAD
+
 import './App.scss';
 import Squares from './Components/017/Squares';
+import Jokes from './Components/testas_09_02/Jokes'
 // import Text from './Components/017/Text';
 // import { useState } from "react";
 // import Show from './Components/017/Show';
@@ -13,36 +14,14 @@ function App() {
 
     return (
         <div className="App">
-            <header className="App-header">
-                {/* <div className="dog-bin">
-                    <div className="dog" style={{backgroundColor: color ? 'crimson' : 'black'}}></div>
-                </div> */}
-                <h1>State Uplifting AND useRef</h1>
-                {/* <Text setShowText={setShowText} />
-                <Show showText={showText} />
-                <Cb setColor={setColor} /> */}
-                <Squares />
-            </header>
+          <header className="App-header">
+            <h1>Ten best jokes</h1>
+            <Jokes color='red'></Jokes>
+          </header>
         </div>
-    );
-=======
-import "./App.scss";
-import Jokes from "./Components/Jokes";
-
-function App() {
+      );
+    }
+    
+    export default App;
 
 
-
-  
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Ten best jokes</h1>
-        <Jokes color='red'></Jokes>
-      </header>
-    </div>
-  );
->>>>>>> f5f0667acec1b33af31a52fe7aa89bd880ab8cf4
-}
-
-export default App;
