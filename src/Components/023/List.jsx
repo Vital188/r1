ac8844806788b7b8efee818208e59e810fd7b86a
Vitalij <vitalij.cernysiov@gmail.com>
2022-09-19 14:35:44 +0300

    Update

--- src/Components/023/List.jsx
+++ src/Components/023/List.jsx
@@ -7,7 +7,8 @@ function List() {
     const { trees } = useContext(TreesProvider);
 
     return (
-        <ul>
+        <ul className="trees">
+            <h2>List</h2>
         {
             trees.map(t => <Row key={t.id} t={t} />)
         }
--- src/Components/023/Row.jsx
+++ src/Components/023/Row.jsx
@@ -1,7 +1,28 @@
-function Row({t}) {
+import { useContext } from "react";
+import TreesProvider from "./TreesProvider";
+
+function Row({ t }) {
+
+    const { types, setDeleteData, setModalData } = useContext(TreesProvider);
+
+    const destroy = () => {
+        setDeleteData({
+            id: t.id
+        });
+    }
+
+    const edit = () => {
+        setModalData({...t});
+    }
 
     return (
-            <li>{t.id} {t.title} <i>{t.height} meters</i></li>
+        <li className="tree-row">
+            <span> {t.title} </span>
+            <span> {types.find(tr => tr.id === t.type).type} </span>
+            <i>{t.height}m</i>
+            <button className="green"onClick={edit}>Edit</button>
+            <button className="red" onClick={destroy}>Delete</button>
+        </li>
     );
 
 }
