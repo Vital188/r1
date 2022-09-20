e9eac3071bdb45c715bdc8b974a52ea350cff427
Vitalij <vitalij.cernysiov@gmail.com>
2022-09-20 18:14:54 +0300

    update

---
+++ src/Components/027/InnerJoin.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function InnerJoin() {
+
+    const [clients, setClients] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3003/get-it/inner-join')
+        .then(res => setClients(res.data));
+    })
+
+    return (
+        <ul className="trees">
+            <h2>InnerJoin</h2>
+        {
+            clients.map(c => <li className="tree-row">
+                <span>ID: {c.id} </span>
+                <span>{c.name}</span>
+                <span>ID: {c.pid} </span>
+                <span>{c.phone}</span>
+            </li>)
+        }
+        </ul>
+    );
+}
+
+export default InnerJoin;
